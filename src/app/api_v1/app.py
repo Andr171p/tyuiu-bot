213fee8a631ab76b92b80da7c0a712016373298b
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-01-22 00:59:19 +0500

    [tyuiu-rag-bot] 22.01.2025 register router

--- src/app/api_v1/app.py
+++ src/app/api_v1/app.py
@@ -3,7 +3,10 @@ import logging
 from fastapi import FastAPI
 
 from src.app.api_v1.lifespan import lifespan
-from src.app.api_v1.routers import wh_router
+from src.app.api_v1.routers import (
+    wh_router,
+    admin_router
+)
 
 
 logging.basicConfig(level=logging.INFO)
@@ -13,6 +16,5 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-app.include_router(
-    wh_router
-)
+app.include_router(wh_router)
+app.include_router(admin_router)
