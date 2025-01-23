68edfad352ed60a3b1f87d4f09cbd7ae02e6ac86
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-01-23 18:04:19 +0500

    [tyuiu-rag-bot] 23.01.2025

--- src/app/api_v1/app.py
+++ src/app/api_v1/app.py
@@ -7,7 +7,8 @@ from src.app.api_v1.lifespan import lifespan
 from src.app.api_v1.routers import (
     wh_router,
     admin_router,
-    statistics_router
+    statistics_router,
+    notification_router
 )
 
 
@@ -23,3 +24,4 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(wh_router)
 app.include_router(statistics_router)
 app.include_router(admin_router)
+app.include_router(notification_router)
--- src/app/api_v1/routers/__init__.py
+++ src/app/api_v1/routers/__init__.py
@@ -1,9 +1,11 @@
 __all__ = (
     "wh_router",
     "admin_router",
-    "statistics_router"
+    "statistics_router",
+    "notification_router"
 )
 
 from src.app.api_v1.routers.webhook import wh_router
 from src.app.api_v1.routers.admin import admin_router
 from src.app.api_v1.routers.statistics import statistics_router
+from src.app.api_v1.routers.notification import notification_router
