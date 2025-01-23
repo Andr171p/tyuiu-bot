d367e8d9ffd421b6cb27f6f4f2026e3f2b6c1463
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-01-23 18:16:01 +0500

    [tyuiu-rag-bot] 23.01.2025 DIP ser up

--- src/app/api_v1/app.py
+++ src/app/api_v1/app.py
@@ -3,6 +3,9 @@ import logging
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from dishka.integrations.fastapi import setup_dishka
+
+from src.app.container import container
 from src.app.api_v1.lifespan import lifespan
 from src.app.api_v1.routers import (
     wh_router,
@@ -25,3 +28,8 @@ app.include_router(wh_router)
 app.include_router(statistics_router)
 app.include_router(admin_router)
 app.include_router(notification_router)
+
+setup_dishka(
+    container=container,
+    app=app
+)
