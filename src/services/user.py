f240d92c779338191fc29831fe2d5a58cf12f28c
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-02-09 17:10:12 +0500

    09.02.2025 fix user register

--- src/services/user.py
+++ src/services/user.py
@@ -12,5 +12,8 @@ class UserService:
     user_repository = UserRepository()
     
     async def register(self, user: UserSchema) -> None:
+        if await self.user_repository.get_by_user_id(user.user_id) is None:
+            log.info("User %s alreasy registered", user.user_id)
+            return
         registered_user = await self.user_repository.add(user)
         log.info("User %s registered successfully", registered_user)
