fc729a9116c30206b90cd2c836ee33d9e35352ea
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-03-18 16:06:02 +0500

    18.03.2025 add phone_number validation

--- src/core/entities/notifications.py
+++ src/core/entities/notifications.py
@@ -1,6 +1,8 @@
 from typing import Any
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from src.misc.formaters import format_phone_number
 
 
 class NotificationAll(BaseModel):
@@ -21,11 +23,19 @@ class NotificationByPhoneNumber(BaseModel):
     text: str
     phone_number: str
 
+    @field_validator("phone_number")
+    def validate_phone_number(cls, phone_number: str) -> str:
+        return format_phone_number(phone_number)
+
 
 class NotificationWithPhotoByPhoneNumber(BaseModel):
     text: str
     photo: Any
     phone_number: str
+
+    @field_validator("phone_number")
+    def validate_phone_number(cls, phone_number: str) -> str:
+        return format_phone_number(phone_number)
 
 
 class NotificationWithPhotoByUserId(BaseModel):
