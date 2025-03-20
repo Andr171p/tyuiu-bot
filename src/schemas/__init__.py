34596a05e092ac779e68ee1e7c143bdf50d9ef2a
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-03-20 19:37:10 +0500

    20.03.2025 fix schemas init

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -13,5 +13,5 @@ __all__ = (
 from src.schemas.users_schemas import UsersResponse, UsersCountResponse
 from src.schemas.contacts_schemas import ContactsResponse, ContactsCountResponse
 from src.schemas.chats_schemas import DialogsResponse, DialogsCountResponse
-from src.schemas.notifications_schemas import DeliveredResponse, DeliveredResponsePresenter
+from src.schemas.delivered_schemas import DeliveredResponse, DeliveredResponsePresenter
 from src.schemas.per_day_distribution_schema import PerDayDistributionResponse
