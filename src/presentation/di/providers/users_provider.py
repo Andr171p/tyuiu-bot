3253d38433666eee04c484ef04a7ebe06b30b3ce
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-04-09 12:42:53 +0500

    09.04.2025 add auth api to users provider

--- src/presentation/di/providers/users_provider.py
+++ src/presentation/di/providers/users_provider.py
@@ -3,17 +3,29 @@ from dishka import Provider, provide, Scope
 from src.repository import UserRepository, ContactRepository, DialogRepository
 from src.core.use_cases import UsersUseCase
 from src.controllers import UsersController
+from src.apis import AuthAPI
+from src.config import settings
 
 
 class UsersProvider(Provider):
     @provide(scope=Scope.APP)
+    def get_auth_api(self) -> AuthAPI:
+        return AuthAPI(settings.auth.base_url)
+
+    @provide(scope=Scope.APP)
     def get_users_use_case(
             self,
-            users_repository: UserRepository,
+            user_repository: UserRepository,
             contact_repository: ContactRepository,
-            dialog_repository: DialogRepository
+            dialog_repository: DialogRepository,
+            auth_api: AuthAPI
     ) -> UsersUseCase:
-        return UsersUseCase(users_repository, contact_repository, dialog_repository)
+        return UsersUseCase(
+            user_repository=user_repository,
+            contact_repository=contact_repository,
+            dialog_repository=dialog_repository,
+            auth_api=auth_api
+        )
 
     @provide(scope=Scope.APP)
     def get_users_controller(self, users_use_case: UsersUseCase) -> UsersController:
